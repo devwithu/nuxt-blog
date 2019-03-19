f1961ed5a3a64ba61cc03e9f49a1b90b5899e993
jdj <jdj@elevisor.com>
2019-03-20 07:48:58 +0900

    3. Preparing Data on the Server-Side

--- pages/index.vue
+++ pages/index.vue
@@ -16,19 +16,28 @@ export default {
   components: {
     PostList
   },
-  data() {
-    return {
-      loadedPosts: [
-        { id: '1', title: 'First Post', previewText: 'This tis our first psot!',
-          thumbnail: 'https://static.coindesk.com/wp-content/uploads/2018/11/shutterstock_1098423464.jpg' },
-        { id: '2', title: 'First2 Post', previewText: 'This tis our first psot!',
-          thumbnail: 'https://static.coindesk.com/wp-content/uploads/2018/11/shutterstock_1098423464.jpg' },
-        { id: '3', title: 'First3 Post', previewText: 'This tis our first psot!',
-          thumbnail: 'https://static.coindesk.com/wp-content/uploads/2018/11/shutterstock_1098423464.jpg' }
+  asyncData(context, callback) {
+    console.log('asyncData is executed')
+    setTimeout(() => {
+      callback(null, {
+        loadedPosts: [
+          { id: '1', title: 'First Post', previewText: 'This tis our first psot!',
+            thumbnail: 'https://static.coindesk.com/wp-content/uploads/2018/11/shutterstock_1098423464.jpg' },
+          { id: '2', title: 'First2 Post', previewText: 'This tis our first psot!',
+            thumbnail: 'https://static.coindesk.com/wp-content/uploads/2018/11/shutterstock_1098423464.jpg' },
+          { id: '3', title: 'First3 Post', previewText: 'This tis our first psot!',
+            thumbnail: 'https://static.coindesk.com/wp-content/uploads/2018/11/shutterstock_1098423464.jpg' }
+        ]
+      })
+    },1500)
+  },
+  // data() {
+  //   return {
+  //     loadedPosts: []
+  //   }
+  // },
+  created() {
 
-
-      ]
-    }
   }
 }
 </script>
