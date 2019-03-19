6cf43bf822d7a871caba74d7a5ff02946e8bd029
jdj <jdj@elevisor.com>
2019-03-19 09:29:48 +0900

    13. adding Separate layout to the admin area

---
+++ layouts/admin.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+html {
+  font-family: 'Nanum Gothic', sans-serif;
+}
+body {
+  margin: 0;
+}
+</style>
