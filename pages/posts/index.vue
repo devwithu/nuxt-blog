15f3e5809da8d480be948ab578f1775ffa2b2077
jdj <jdj@elevisor.com>
2019-03-20 07:53:36 +0900

    asyncData on Client and Server

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="posts-page">
-    <PostList />
+    <PostList :posts="loadedPosts" />
  </div>
 </template>
 
@@ -10,7 +10,22 @@ import PostList from '@/components/Posts/PostList'
 export default {
   components: {
     PostList
-  }
+  },
+  asyncData(context, callback) {
+    console.log('asyncData is executed')
+    setTimeout(() => {
+      callback(null, {
+        loadedPosts: [
+          { id: '1', title: 'First Post', previewText: 'This tis our first psot!',
+            thumbnail: 'https://static.coindesk.com/wp-content/uploads/2018/11/shutterstock_1098423464.jpg' },
+          { id: '2', title: 'First2 Post', previewText: 'This tis our first psot!',
+            thumbnail: 'https://static.coindesk.com/wp-content/uploads/2018/11/shutterstock_1098423464.jpg' },
+          { id: '3', title: 'First3 Post', previewText: 'This tis our first psot!',
+            thumbnail: 'https://static.coindesk.com/wp-content/uploads/2018/11/shutterstock_1098423464.jpg' }
+        ]
+      })
+    },1500)
+  },  
 }
 </script>
 
