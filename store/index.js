6c978959159cad6e7e04e011e21833352655422d
jdj <jdj@elevisor.com>
2019-03-26 08:48:24 +0900

    5. Fetching Data from the Backend

--- store/index.js
+++ store/index.js
@@ -1,4 +1,5 @@
 import Vuex from 'vuex'
+import axios from 'axios'
 
 const createStore = () => {
     return new Vuex.Store({
@@ -12,22 +13,15 @@ const createStore = () => {
         },
         actions: {
             nuxtServerInit(vuexContext, context) {
-                // if (!process.client) {
-                //     console.log(context.req)
-                // }
-                return new Promise((resolve, reject) => {
-                  setTimeout(() => {
-                    vuexContext.commit("setPosts", [
-                        { id: '1', title: 'First Post', previewText: 'This tis our first psot!',
-                          thumbnail: 'https://static.coindesk.com/wp-content/uploads/2018/11/shutterstock_1098423464.jpg' },
-                        { id: '2', title: 'First2 Post', previewText: 'This tis our first psot!',
-                          thumbnail: 'https://static.coindesk.com/wp-content/uploads/2018/11/shutterstock_1098423464.jpg' },
-                        { id: '3', title: 'First3 Post', previewText: 'This tis our first psot!',
-                          thumbnail: 'https://static.coindesk.com/wp-content/uploads/2018/11/shutterstock_1098423464.jpg' }
-                      ]);
-                    resolve();
-                  }, 1000);
-                });
+                return axios.get('https://nestjsandfirebase.firebaseio.com/posts.json')
+                    .then(res => {
+                        const postsArray = []
+                        for (const key in res.data) {
+                            postsArray.push({ ...res.data[key], id: key })
+                        }
+                        vuexContext.commit('setPosts', postsArray)
+                    })
+                    .catch(e => context.error(e));
             },
             setPosts(vuexContext, posts) {
                 vuexContext.commit('setPosts', posts)
