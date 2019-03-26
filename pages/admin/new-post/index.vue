26f98166d0089c2f780fac663593f0956d2b3490
jdj <jdj@elevisor.com>
2019-03-26 10:02:47 +0900

    10. Synchronizing Vuex and the Backend

--- pages/admin/new-post/index.vue
+++ pages/admin/new-post/index.vue
@@ -17,14 +17,12 @@ export default {
   },
   methods: {
     onSubmitted(postData) {
-      axios.post('https://nestjsandfirebase.firebaseio.com/posts.json', { 
-        ...postData, 
-        updatedDate: new Date()
-        })
-        .then(result =>  {
+      this.$store.dispatch('addPost', postData)
+        .then( () => {
           this.$router.push('/admin')
-        })
-        .catch(e => console.log(e));
+        }
+
+        )
     }
   }
 }
