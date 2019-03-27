718fda9fe6627afd154bada6162f97852d0e2319
jdj <jdj@elevisor.com>
2019-03-28 08:55:52 +0900

    8. Invalidating the Token

--- store/index.js
+++ store/index.js
@@ -22,6 +22,9 @@ const createStore = () => {
             },
             setToken(state, token) {
                 state.token = token
+            },
+            clearToken(state) {
+                state.token = null
             }
         },
         actions: {
@@ -73,9 +76,15 @@ const createStore = () => {
                     returnSecureToken: true
                   }
                 ).then(result => {
-                  vuexContext.commit('setToken', result.idToken)
+                  vuexContext.commit('setToken', result.idToken);
+                  vuexContext.dispatch('setLogoutTimer', result.expiresIn * 1000)
                 })
                 .catch(e => console.log(e));                  
+            },
+            setLogoutTimer(vuexContext, duration) {
+                setTimeout(() => {
+                    vuexContext.commit('clearToken');
+                }, duration)
             }
         },
         getters: {
